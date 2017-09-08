7e99d121aafbe534757425145411595349d3f448
wulivicte <676715563@qq.com>
2017-09-08 08:48:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-cd /opt/deepdetect/build/main && (nohup ./dede -host 0.0.0.0 &) && python /home/objdetect.py
+cd /opt/deepdetect/build/main && (nohup ./dede -host 0.0.0.0 &) && cd /home && python objdetect.py
